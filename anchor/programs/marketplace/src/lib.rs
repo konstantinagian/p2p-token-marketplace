fb324a82d595baeb2a4adbd2f4c3d1b2eeda2c37
konstantinagian <102831866+konstantinagian@users.noreply.github.com>
2024-01-17 16:00:53 +0200

    Add listing instruction

--- anchor/programs/marketplace/src/lib.rs
+++ anchor/programs/marketplace/src/lib.rs
@@ -1,10 +1,11 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::Token;
+use anchor_spl::{token::{Token, Mint, TokenAccount, Transfer, transfer}, associated_token::AssociatedToken};
 
 declare_id!("2xnGNsckC4PcvmDUmUz7dM9hNHCAFmxfk8W3iT8TX4iQ");
 
 #[program]
 pub mod marketplace {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
@@ -15,6 +16,25 @@ pub mod marketplace {
         });
 
         Ok(())
+    }
+
+    pub fn list(ctx: Context<List>, price: u64, amount: u64) -> Result<()> {
+        ctx.accounts.listing.set_inner(Listing {
+            maker: ctx.accounts.maker.key(),
+            mint: ctx.accounts.mint.key(),
+            price,
+        });
+
+        // Send the token into the listing vault
+        let transfer_accounts = Transfer {
+            from: ctx.accounts.maker_ata.to_account_info(),
+            to: ctx.accounts.listing_vault.to_account_info(),
+            authority: ctx.accounts.maker.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), transfer_accounts);
+
+        transfer(cpi_ctx, amount)
     }
 }
 
@@ -40,10 +60,47 @@ pub struct Initialize<'info> {
     token_program: Program<'info, Token>,
 }
 
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct List<'info> {
+    #[account(mut)]
+    maker: Signer<'info>,
+    mint: Account<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = maker
+    )]
+    maker_ata: Account<'info, TokenAccount>,
+    #[account(
+        seeds = [b"marketplace", name.as_str().as_bytes()],
+        bump
+    )]
+    marketplace: Account<'info, Marketplace>,
+    #[account(
+        init,
+        payer = maker,
+        space = Listing::INIT_SPACE,
+        seeds = [b"listing", marketplace.key().as_ref()],
+        bump
+    )]
+    listing: Account<'info, Listing>,
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = mint,
+        associated_token::authority = listing
+    )]
+    listing_vault: Account<'info, TokenAccount>,
+    associated_token_program: Program<'info, AssociatedToken>,
+    system_program: Program<'info, System>,
+    token_program: Program<'info, Token>,
+}
+
 #[account]
 pub struct Marketplace {
     admin: Pubkey,
-    fee: u16, // if we want to charge a fee in each sale
+    fee: u16, // if we want to charge a fee in each sale (percentage)
     name: String,
 }
 
