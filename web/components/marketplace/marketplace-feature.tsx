6b47bf812ecd9036811272ed51a0efa389f5d76c
konstantinagian <102831866+konstantinagian@users.noreply.github.com>
2024-01-17 22:02:53 +0200

    Add listing token model

--- web/components/marketplace/marketplace-feature.tsx
+++ web/components/marketplace/marketplace-feature.tsx
@@ -1,23 +1,37 @@
 'use client';
 
+import { useState } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletButton } from '../solana/solana-provider';
 import { AppHero } from '../ui/ui-layout';
+import { ListingModal } from './marketplace-ui';
 // import { useMarketplaceProgram } from './marketplace-data-access';
 
 export default function MarketplaceFeature() {
   const { publicKey } = useWallet();
 //   const { programId } = useMarketplaceProgram();
+    const [showModal, setShowModal] = useState(false);
 
   return publicKey ? (
-    <div>
+    <div className='text-center'>
       <AppHero
         title="Marketplace"
         subtitle={
           'List your SPL tokens for other users to buy'
         }
       >
-      </AppHero>
+        </AppHero>
+        <button
+          className="btn btn-xs lg:btn-md btn-primary"
+          onClick={() => setShowModal(true)}
+        >
+          + List Token
+        </button>
+        <ListingModal
+          show={showModal}
+          hideModal={() => setShowModal(false)}
+          address={publicKey}
+        />
     </div>
   ) : (
     <div className="max-w-4xl mx-auto">
---
+++ web/components/marketplace/marketplace-ui.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { PublicKey } from '@solana/web3.js';
+import { useState, useMemo } from 'react';
+import { AppModal } from '../ui/ui-layout';
+import { useGetTokenAccounts } from '../account/account-data-access';
+
+export function ListingModal({
+    hideModal,
+    show,
+    address
+  }: {
+    hideModal: () => void;
+    show: boolean;
+    address: PublicKey;
+  }) {
+
+    const query = useGetTokenAccounts({ address });
+    const items = useMemo(() => {
+      return query.data;
+    }, [query.data]);
+
+    // const [tokenAccount, setTokenAccount] = useState('');
+    // const [tokenMint, setTokenMint] = useState('');
+    const [itemIndex, setItemIndex] = useState(0);
+
+    return (
+      <AppModal
+        title={'List Token'}
+        hide={hideModal}
+        show={show}
+        submit={() => {
+        //   listToken();
+          hideModal();
+        }}
+        submitLabel="List"
+      >
+        <select
+          className="select select-bordered w-full"
+          // value={tokenAccount ?? 0}
+          onChange={e => {
+            setItemIndex(e.target.selectedIndex)
+            // setTokenAccount(e.target.value)
+            // setTokenMint(e.target.querySelectorAll('option')[e.target.selectedIndex].getAttribute('data-mint') ?? '')
+          }}
+        >
+          <option value={undefined}>Select a token</option>
+          {items?.map(({ account, pubkey }, index) =>
+            <option value={pubkey.toString()} key={index} data-mint={account.data.parsed.info.mint.toString()}>{account.data.parsed.info.mint.toString()}</option>)}
+
+        </select>
+
+        {items && itemIndex > 0 && (
+          <div className='text-left'>
+            <h2 className='font-bold'>Mint:</h2>
+            <div className=''>{items[itemIndex - 1].pubkey.toString()}</div>
+            <h2 className='font-bold'>Token Account:</h2>
+            <div className=''>{items[itemIndex - 1].account.data.parsed.info.mint.toString()}</div>
+            {/* <h2 className='font-bold'>Your Balance:</h2>
+            <div className=''>{balance}</div> */}
+          </div>
+        )}
+      </AppModal>
+    );
+  }
